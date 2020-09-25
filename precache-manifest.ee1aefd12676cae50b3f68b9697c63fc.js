d70c8a4dd341272195b4ae23e18018142a3fc0e5
Hardikpoudel <hardikpoudel25@gmail.com>
2020-09-25 15:26:11 +0545

    Updates

---
+++ precache-manifest.ee1aefd12676cae50b3f68b9697c63fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63c5a34ecd00524c91de8571cc762d3",
+    "url": "/Github-Profilecard/index.html"
+  },
+  {
+    "revision": "b0d2ddda5a06a2feeeca",
+    "url": "/Github-Profilecard/static/css/main.0a2a480c.chunk.css"
+  },
+  {
+    "revision": "53c9bc003ffe4c9b5a3a",
+    "url": "/Github-Profilecard/static/js/2.4387b342.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Github-Profilecard/static/js/2.4387b342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0d2ddda5a06a2feeeca",
+    "url": "/Github-Profilecard/static/js/main.59dbd406.chunk.js"
+  },
+  {
+    "revision": "efd1c20e2363c3957dd4",
+    "url": "/Github-Profilecard/static/js/runtime-main.8f4dd20d.js"
+  }
+]);
